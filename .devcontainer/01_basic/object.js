8c5f871bf3ea24dd7b26a504f5eb25a467dd83ab
Atul Kumar <105835497+AtulKrsna16@users.noreply.github.com>
2023-09-10 16:48:27 +0000

    object done

---
+++ .devcontainer/01_basic/object.js
@@ -0,0 +1,7 @@
+// how to create object
+const person = {name : "atul" , age : 25}
+console.log(person);
+console.log(person.age);
+console.log(person.name);
+console.log(person["name"]); //this is best way
+
